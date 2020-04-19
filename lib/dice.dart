4bd382fd82865a72f24c81b9f2ab8317ee7845e0
IAkinola <ilerioluwaakinola@gmail.com>
2020-04-20 01:46:47 +0200

    Update dice.dart

--- lib/dice.dart
+++ lib/dice.dart
@@ -4,7 +4,35 @@ class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Expanded(
+                    child: Image.asset('images/dice5.png'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Expanded(
+                    child: Image.asset('images/dice2.png'),
+                  ),
+                ),
+              ],
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text(
+                'roll',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
