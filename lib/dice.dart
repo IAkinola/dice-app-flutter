2d4d19bc7b7fcb27f58663f6ac185000a64b1976
IAkinola <ilerioluwaakinola@gmail.com>
2020-04-21 11:52:06 +0200

    Added function to change dice face.

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  //function to change dice face
+  void throwFunction() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,22 +26,31 @@ class DicePage extends StatelessWidget {
           children: <Widget>[
             Row(
               children: <Widget>[
+                //Left Die
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Expanded(
-                    child: Image.asset('images/dice5.png'),
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
                   ),
                 ),
+
+                //Right Die
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Expanded(
-                    child: Image.asset('images/dice2.png'),
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
                   ),
                 ),
               ],
             ),
+
+            //Button to throw dice
             RaisedButton(
-              onPressed: () {},
+              color: Colors.white,
+              textColor: Colors.red,
+              onPressed: () {
+                throwFunction();
+              },
               child: Text(
                 'roll',
                 style: TextStyle(fontSize: 20),
